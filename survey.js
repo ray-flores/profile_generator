28810cd2b776ef03379ef955a4935f9656ef0d53
Ray Flores <ray.m.flores@gmail.com>
2021-11-30 23:40:15 +0000

    moved profile template into one line to create a nicely formatted para on the terminal

--- survey.js
+++ survey.js
@@ -12,9 +12,7 @@ rl.question('What\'s your name? Nicknames are also acceptable ', (answer1) => {
         rl.question('What\'s your favourite thing to eat for that meal? ', (answer5) => {
           rl.question('Which sport is your absolute favourite? ', (answer6) => {
             rl.question('What is your superpower? ', (answer7) => {
-              console.log(`${answer1} loves listening to ${answer3} while ${answer2},
-              devouring ${answer5} for ${answer4}, prefers ${answer6} over any other sport,
-              and is amazing at ${answer7}.`);
+              console.log(`${answer1} loves listening to ${answer3} while ${answer2}, devouring ${answer5} for ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}.`);
               rl.close();
             });
           });
